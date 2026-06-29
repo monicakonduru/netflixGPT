74a76fe6801fb346c7a5d6c4d104103f4f50f61e
Monica <monica@luxhospitals.com>
2026-06-29 15:57:09 +0530

    Created a seperate constants file for all the constant values

---
+++ src/utils/constants.js
@@ -0,0 +1,29 @@
+// Shared static assets and config used across the app.
+
+// Netflix wordmark logo (used in the header and signup page).
+export const NETFLIX_LOGO =
+  'https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2026-05-14/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
+
+// Full-bleed background image on the login and signup screens (served from /public).
+export const LOGIN_BG = '/loginBg.png'
+
+// Default avatar shown in the header for a signed-in user (served from /public).
+export const AVATAR = '/user.png'
+
+// Header navigation links.
+export const NAV_LINKS = [
+  'Home',
+  'Shows',
+  'Movies',
+  'Games',
+  'New & Popular',
+  'My List',
+  'Browse by Languages',
+]
+
+// Static profiles shown in the avatar dropdown, mirroring the Netflix UI.
+export const PROFILES = [
+  { name: 'Profile 1', img: '/profile1.png' },
+  { name: 'Profile 2', img: '/profile1.png' },
+  { name: 'Children', color: 'bg-gradient-to-br from-pink-400 to-yellow-300', kids: true },
+]
